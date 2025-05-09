8b2661f4c3ef675a710767d87866df4167822fe0
kaytech <kayodeoloyede09911@gmail.com>
2025-05-09 13:05:31 +0100

    my portfolio

--- Kayportfolio/style.js
+++ Kayportfolio/style.js
@@ -11,3 +11,28 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+var navlinks = document.getElementById("nav-links");
+function openmenu() {
+  navlinks.style.right = "0";
+}
+function closemenu() {
+  navlinks.style.right = "-200px";
+}
+
+const menu = document.getElementById("menu");
+const navLinks = document.getElementById("nav-links");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".typed", {
+    strings: ["Designer", "Developer", "Freelancer", "Creator"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+  });
+});
